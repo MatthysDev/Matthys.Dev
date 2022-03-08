136e0afb56ac27251a30e5b144ccc50ce7563777
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2022-03-09 00:49:35 +0100

    add talk in slides closes #53

--- src/pages/slides.tsx
+++ src/pages/slides.tsx
@@ -5,6 +5,26 @@ import Layout from "../components/layout";
 const SlidesPage = () => {
   return (
     <Layout pageTitle={"Slides"}>
+      <div className="sm:flex-col md:flex md:flex-row m-auto h-2/3 my-8">
+        <div className="m-auto h-2/3 sm:w-5/6 md:w-1/2 p-4 ">
+          <p className="text-center pt-4  pl-2 pb-4 text-gray-400 hover:text-purple-400 text-2xl">
+            <a
+              href="https://react-native-heroes.netlify.app/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Discover how I build a fullstack serveless application
+            </a>
+          </p>
+        </div>
+
+        <StaticImage
+          className="border-purple-400 border-2 w-2/3 h-2/3 md:w-2/5 md:h-2/5 m-auto"
+          src="../images/talk.jpg"
+          alt={"img"}
+        />
+      </div>
+      <hr className="border-2 border-green-400 w-1/5 m-auto" />
       <div className="sm:flex-col md:flex md:flex-row m-auto h-2/3 my-8 ">
         <div className="m-auto h-2/3 sm:w-5/6 md:w-1/2 ">
           <p className="pt-4 text-center pl-2 pb-4 text-gray-400 hover:text-purple-400 text-2xl">
@@ -45,7 +65,6 @@ const SlidesPage = () => {
           alt={"img"}
         />
       </div>
-      <hr className="border-2 border-green-400 w-1/5 m-auto" />
     </Layout>
   );
 };
